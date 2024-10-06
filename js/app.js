a8d793817de6f032ccc8374cff503d0deafce691
fardinariful <fardinewucse2020@gmail.com>
2024-10-06 10:33:35 +0600

    something added

--- js/app.js
+++ js/app.js
@@ -8,10 +8,53 @@ const allpet = async() =>{
 
 const displayitems = items =>{
     //console.log(items);
-
+    const allitems=document.getElementById("all_items");
     items.forEach(item =>{
         console.log(item);
     })
+
+  
+    const div1=document.createElement('div');
+    div1.classList=`mt-10  flex flex-row justify-between gap-3`;
+    div1.innerHTML=`
+     <div class="border border-[#0E7A8126] px-3 py-1">
+            
+            <p class="mt-4">Mister Tartosh</p>
+            <div class="mt-2 space-y-2">
+                <div class="flex flex-row gap-2">
+                    <img src="./images/Frame.jpg" alt="">
+                    <p><span>Breed:</span>  Golder retriever</p>
+                </div>
+                <div class="flex flex-row gap-2">
+                    <img src="./images/Frame (1).png" alt="">
+                    <p><span>Birth:</span>2024</p>
+                </div>
+                <div class="flex flex-row gap-2">
+                    <img src="./images/Frame (2).png" alt="">
+                    <p><span>Gender:</span> Female</p>
+                </div>
+                <div class="flex flex-row gap-2">
+                    <img src="./images/Frame (3).png" alt="">
+                    <p><span>Price:</span> 199$</p>
+                </div>
+            </div>
+            <hr class="mt-3">
+            <div class="flex flex-row justify-between items-center mt-3">
+                <button class="border border-[#13131399] px-3 py-1 rounded-xl">
+                    <img src="./images/Frame (4).png" alt="">
+                </button>
+                <button  class="border border-[#13131399] px-3 py-1 text-[#0E7A81] font-medium rounded-xl">Adopt</button>
+                <button  class="border border-[#13131399] px-3 py-1 text-[#0E7A81] font-medium rounded-xl">Details</button>
+            </div>
+            
+        </div>
+    
+    
+    `
+
+   allitems.appendChild(div1);
+
+
 }
 
 
