05775c45f1c31ceb7151f9a9031cbc95b9fdb1f0
fardinariful <fardinewucse2020@gmail.com>
2024-10-06 10:19:27 +0600

    all items shows in inspect

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 const allpet = async() =>{
     const resp = await fetch("https://openapi.programming-hero.com/api/peddy/pets");
     const data=await resp.json();
-    console.log(data.pets);
+    const alldata=data.pets;
+    //console.log(alldata);
+    displayitems(alldata);
 }
+
+const displayitems = items =>{
+    //console.log(items);
+
+    items.forEach(item =>{
+        console.log(item);
+    })
+}
+
+
 
 allpet();
